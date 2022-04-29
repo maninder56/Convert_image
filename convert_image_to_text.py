9ecd60e5edde066943b527d497f092284c9c1504
maninder56 <maninder123450@gmail.com>
2022-04-29 23:13:17 +0100

    get photo improved

--- convert_image_to_text.py
+++ convert_image_to_text.py
@@ -2,7 +2,8 @@ from PIL import Image
 import os
 
 path = 'C:/Users/User/PycharmProjects/Convert_image_to_text/'
-photo_path = 'Images/heart_1.png'
+photo_path = 'Images/wolf_icon.jpg'
+
 
 
 # photo size = 25x25 = 625
@@ -12,7 +13,7 @@ def get_photo_rgb(image_path):
     photo = Image.open(path + image_path)
     for row in range(photo.size[1]):  # get the rgb data of every pixel
         for col in range(photo.size[0]):
-            pixel_data = photo.getpixel((col, row))
+            pixel_data = photo.getpixel((col,row))
             rgb_list.append(pixel_data)
 
     print("\nrgb_list length = ", len(rgb_list))
@@ -36,29 +37,29 @@ def assing_character(brightness_data):
     brightness_list = brightness_level(brightness_data)
     for item in brightness_list:
         if 255 >= item > 234:  # 255 to 233
-            assigned_character.append('.')
+            assigned_character.append(' ..')
         elif 234 >= item > 212:  # 234 to 211
-            assigned_character.append(',')
+            assigned_character.append(' ,,')
         elif 212 >= item > 191:  # 212 to 190
-            assigned_character.append('-')
+            assigned_character.append(' --')
         elif 191 >= item > 170:
-            assigned_character.append('~')
+            assigned_character.append(' ~~')
         elif 170 >= item > 150:
-            assigned_character.append(':')
+            assigned_character.append(' ::')
         elif 150 >= item > 129:
-            assigned_character.append(';')
+            assigned_character.append(' ;;')
         elif 129 >= item > 108:
-            assigned_character.append('=')
+            assigned_character.append(' ==')
         elif 108 >= item > 87:
-            assigned_character.append('!')
+            assigned_character.append(' !!')
         elif 87 >= item > 66:
-            assigned_character.append('*')
+            assigned_character.append(' **')
         elif 66 >= item > 45:
-            assigned_character.append('#')
+            assigned_character.append(' ##')
         elif 44 >= item > 25:
-            assigned_character.append('$')
+            assigned_character.append(' $$')
         else:
-            assigned_character.append('@')
+            assigned_character.append(' @@')
     return assigned_character
 
 
@@ -74,7 +75,7 @@ def save_file(file):
     text.close()
 
 
-print(save_file(photo_path))
+save_file(photo_path)
 
 
 def read_open_text_file():
