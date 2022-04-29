565f91ab163dac155ca82df3d2d8b032df1165fe
maninder56 <maninder123450@gmail.com>
2022-04-29 15:01:44 +0100

    added function to calculate average brightness

--- convert_image_to_text.py
+++ convert_image_to_text.py
@@ -2,46 +2,44 @@ from PIL import Image
 import os
 
 path = 'C:/Users/User/PycharmProjects/Convert_image_to_text/'
+photo_path = 'Images/flower_small.jpg'
 
-#photo size = 25x25 = 625
+
+# photo size = 25x25 = 625
 
 def get_photo_rgb(image_path):
-    rgb_list = []# store all rbg data
-    rgb_table = [] # rgb data in sub list
+    rgb_list = []  # store all rbg data
     photo = Image.open(path + image_path)
-    for row in range(photo.size[0]):#get the rgb data of every pixel
+    for row in range(photo.size[0]):  # get the rgb data of every pixel
         for col in range(photo.size[1]):
-            pixel_data = photo.getpixel((row,col))
+            pixel_data = photo.getpixel((row, col))
             rgb_list.append(pixel_data)
-
-    for i in range(0 , len(rgb_list), photo.size[0]):#convert rgb list in sub list
-        chunk = rgb_list[i : i + photo.size[0]]
-        rgb_table.append(chunk)
+    '''
+    for i in range(0, len(rgb_list), photo.size[0]):  # convert rgb list in sub list
+        chunk = rgb_list[i: i + photo.size[0]]
+        rgb_table.append(chunk)'''
 
     print("\nrgb_list length = ", len(rgb_list))
-    print("\nrgb_list length = ", len(rgb_table))
     photo.close()
 
-    return rgb_table
-
-get_photo_rgb('Images/flower_small.jpg')
+    return rgb_list
 
 
-#def brightness_nevel(rgb_list)
+def brightness_level(photo):
+    brightness_list = []
+    rgb_list = get_photo_rgb(photo)
+    for rgb in rgb_list:
+        average_value = (rgb[0] + rgb[1] + rgb[2]) / 3
+        brightness_list.append(round(average_value))
+    return brightness_list
 
-
-
-
-
-
-
-
+brightness_level(photo_path)
 
 
 def read_open_text_file():
     text = open(path + 'Convert_image/Text/wolf.txt', 'r')
-    print('\n',text.read())
+    print('\n', text.read())
     text.close()
     # os.startfile(path + 'Convert_image/Text/wolf.txt')
 
-#read_open_text_file()
+# read_open_text_file()
