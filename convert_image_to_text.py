670f5ca551d8245fa37b82daefe4056a4bb9d688
maninder56 <maninder123450@gmail.com>
2022-04-30 17:44:16 +0100

    added main function

--- convert_image_to_text.py
+++ convert_image_to_text.py
@@ -3,8 +3,8 @@ import os
 
 path = 'C:/Users/User/PycharmProjects/Convert_image_to_text/'
 
-photo_format = '.jpg'
-photo_name = 'test'
+photo_format = '.jpeg'
+photo_name = 'thums_up'
 
 photo_path = 'Images/' + photo_name + photo_format
 
@@ -14,24 +14,21 @@ photo_path = 'Images/' + photo_name + photo_format
 # photo size = 25x25 = 625
 
 def get_photo_rgb(image_path):
-    rgb_list = []  # store all rbg data
+    rgb_list = []                           # store all rbg data
     photo = Image.open(path + image_path)
-    for col in range(photo.size[1]):  # get the rgb data of every pixel
+    for col in range(photo.size[1]):        # get the rgb data of every pixel
         for row in range(photo.size[0]):
             pixel_data = photo.getpixel((row, col))
             rgb_list.append(pixel_data)
-
     print("\nrgb_list length = ", len(rgb_list))
     print('Photo sizes', photo.size)
     photo.close()
-
     return rgb_list
 
 
 def brightness_level(photo_data):
     brightness_list = []
-    rgb_list = get_photo_rgb(photo_data)
-    for rgb in rgb_list:
+    for rgb in photo_data:
         average_value = (rgb[0] + rgb[1] + rgb[2]) / 3
         brightness_list.append(round(average_value))
     return brightness_list
@@ -39,13 +36,12 @@ def brightness_level(photo_data):
 
 def assing_character(brightness_data):
     assigned_character = []
-    brightness_list = brightness_level(brightness_data)
-    for item in brightness_list:
-        if 255 >= item > 234:  # 255 to 233
+    for item in brightness_data:
+        if 255 >= item > 234:               # 255 to 233
             assigned_character.append('...')
-        elif 234 >= item > 212:  # 234 to 211
+        elif 234 >= item > 212:             # 234 to 211
             assigned_character.append(',,,')
-        elif 212 >= item > 191:  # 212 to 190
+        elif 212 >= item > 191:             # 212 to 190
             assigned_character.append('---')
         elif 191 >= item > 170:
             assigned_character.append('~~~')
@@ -68,9 +64,8 @@ def assing_character(brightness_data):
     return assigned_character
 
 
-def save_file(file):
+def save_file(save_file):
     photo = Image.open(path + photo_path)
-    save_file = assing_character(file)
     text = open(path + 'Convert_image/Text/' + photo_name + '.txt', 'w')
     for i in range(0, len(save_file), photo.size[0]):
         for x in save_file[i: i + photo.size[0]]:
@@ -80,14 +75,16 @@ def save_file(file):
     text.close()
 
 
-save_file(photo_path)
-
-
-def read_open_text_file():
-    # text = open(path + 'Convert_image/Text/wolf.txt', 'r')
-    # print('\n', text.read())
-    # text.close()
+def open_text_file():
     os.startfile(path + 'Convert_image/Text/' + photo_name + '.txt')
 
 
-read_open_text_file()
+def main(photo):
+    photo_rgb_data = get_photo_rgb(photo)                       # get rgb data of photo
+    brightness_value = brightness_level(photo_rgb_data)         # convert rgb to brightness level
+    assigned_characters = assing_character(brightness_value)    # assign characters to brightness level
+    save_file(assigned_characters)                              # save characters in text file
+    open_text_file()                                            # show saved file
+
+
+main(photo_path)
